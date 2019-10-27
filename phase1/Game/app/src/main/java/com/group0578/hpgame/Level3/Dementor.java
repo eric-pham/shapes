6b40068966c171104d78effcee3438018af823d6
Purvi Nagpal <purvi.nagpal@mail.utoronto.ca>
2019-10-27 16:00:40 -0400

    "Modified class Dementor - added instance attributes to store appearance, position and life status of a Dementor and added methods"

--- phase1/Game/app/src/main/java/com/group0578/hpgame/Level3/Dementor.java
+++ phase1/Game/app/src/main/java/com/group0578/hpgame/Level3/Dementor.java
@@ -1,4 +1,49 @@
 package com.group0578.hpgame.Level3;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+
+import  android.graphics.Color;
+
 public class Dementor {
+    private String appearance;
+    private int x;
+    private int y;
+    private boolean isAlive;
+    private Paint paintText = new Paint();
+
+    Dementor(){
+        this.appearance = "<OO>";
+        this.isAlive = true;
+        paintText.setTextSize(36);
+        paintText.setColor(Color.WHITE);
+        paintText.setTypeface(Typeface.DEFAULT_BOLD);
+    }
+
+    void setLocation(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    int getRow(){
+        return this.y;
+    }
+
+    int getColumn(){
+        return this.x;
+    }
+
+    void move(){
+        this.y += 4;
+    }
+
+    void kill(){
+        this.isAlive = false;
+    }
+
+
+
+
+
 }
