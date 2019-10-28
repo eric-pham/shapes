039bba91cf9510ec30a42478366b90f138ac297e
Henry Wang <henryhe.wang@mail.utoronto.ca>
2019-10-28 01:35:27 -0400

    Added GSON dependencies and debug code

--- phase1/Game/app/src/main/java/com/group0578/hpgame/presenter/LoginPresenter.java
+++ phase1/Game/app/src/main/java/com/group0578/hpgame/presenter/LoginPresenter.java
@@ -2,6 +2,11 @@ package com.group0578.hpgame.presenter;
 
 import com.group0578.hpgame.view.Login;
 
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.io.FileReader;
+import com.google.gson.Gson;
+
 /**
  * Responsible for handling actions from the View {@link com.group0578.hpgame.view.LoginActivity}
  * and updating the UI as required.
@@ -26,6 +31,16 @@ public class LoginPresenter implements Login.Presenter {
         //do something
         System.out.println(username);
         System.out.println(password);
+
+
+        Gson gson = new Gson();
+        try{
+            HashMap hmap = gson.fromJson(new FileReader("userInfo.json"), HashMap.class);
+            System.out.println(hmap);
+            System.out.println(hmap.get("username"));
+        } catch(FileNotFoundException e){
+            System.out.println("Exception!");
+        }
         return true;
     }
 }
