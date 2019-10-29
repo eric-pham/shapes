2012fe49a4504483f519f0de67607cd721080b33
VG <vassilisa.gotcheva@mail.utoronto.ca>
2019-10-29 13:02:49 -0400

    Modified MazeView.java to draw the Maze, however the code does not work and I am still in the process of debugging it.

--- phase1/Game/app/src/main/java/com/group0578/hpgame/Level2/MazeView.java
+++ phase1/Game/app/src/main/java/com/group0578/hpgame/Level2/MazeView.java
@@ -3,16 +3,17 @@ package com.group0578.hpgame.Level2;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.util.AttributeSet;
+import android.graphics.Paint;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-import java.lang.reflect.Array;
 
 /**
  * The Maze's view or visual appearance on the screen for the user.
  */
-public class MazeView extends SurfaceView implements SurfaceHolder.Callback, Maze.View {
+
+public class MazeView extends SurfaceView implements SurfaceHolder.Callback, Maze.View, Runnable {
 
     /**
      * The part of the program managing time.
@@ -28,6 +29,8 @@ public class MazeView extends SurfaceView implements SurfaceHolder.Callback, Maz
      * The SurfaceHolder object controls actions of the display surface of this MazeView.
      */
     private SurfaceHolder surfaceHolder = null;
+    private Paint mazeBrush;
+    Canvas mazeCanvas;
 
     /**
      * The array representing the maze; structured as a grid where each element is a MazeSection.
@@ -37,12 +40,22 @@ public class MazeView extends SurfaceView implements SurfaceHolder.Callback, Maz
     /**
      * The number of rows in the maze.
      */
+
+    // the number of rows in the maze
+
     private static final int ROWS = 7;
 
     /**
      * The number of columns in the maze.
      */
     private static final int COLS = 4;
+
+    private float mazeSectionLength;
+
+    private float verticalMargin;
+
+    private float horizontalMargin;
+
 
     /**
      * Create a new Maze in the context environment.
@@ -58,6 +71,10 @@ public class MazeView extends SurfaceView implements SurfaceHolder.Callback, Maz
         thread = new MainThread(surfaceHolder, this);
         mazePresenter = new MazePresenter(this);
         this.setBackgroundColor(Color.BLACK);
+
+        // Array of MazeSection objects that will represent the maze.
+        mazeGrid = new MazeSection[ROWS][COLS];
+
     }
 
     /**
@@ -71,7 +88,99 @@ public class MazeView extends SurfaceView implements SurfaceHolder.Callback, Maz
      */
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        createMazeGrid();
+        prepareMazeBrush();
+        determineMazeDimensions();
+        drawMazeWalls();
+    }
 
+    private void createMazeGrid() {
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                mazeGrid[row][col] = new MazeSection(row, col);
+            }
+        }
+    }
+
+    private void prepareMazeBrush() {
+        mazeBrush = new Paint();
+        mazeBrush.setColor(Color.WHITE);
+        mazeBrush.setStrokeWidth(3);
+    }
+
+    private void determineMazeDimensions() {
+        mazeCanvas = surfaceHolder.lockCanvas();
+        if (mazeCanvas == null) {
+            String TAG = "MazeView.determineMazeDimensions";
+            Log.e(TAG, "Cannot draw onto the canvas as it's null");
+        } else {
+            int canvasHeight = getHeight();
+            int canvasWidth = getWidth();
+
+            if (canvasHeight/canvasWidth < ROWS/COLS) {
+                mazeSectionLength = canvasHeight/(ROWS + 1);
+            } else {
+                mazeSectionLength = canvasHeight/(ROWS + 1);
+            }
+
+            float mazeWidth = COLS*mazeSectionLength;
+            float mazeHeight = ROWS*mazeSectionLength;
+
+            verticalMargin = (canvasHeight - mazeHeight)/2;
+            horizontalMargin = (canvasWidth - mazeWidth)/2;
+            surfaceHolder.unlockCanvasAndPost(mazeCanvas);
+        }
+    }
+
+    private void drawMazeWalls() {
+        String TAG = "MazeView.drawMazeWalls";
+        Log.i(TAG, "Trying to draw...");
+
+        mazeCanvas.translate(horizontalMargin, verticalMargin);
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                if (mazeGrid[row][col].getHasTopWall()) {
+                    drawTopWall(row, col);
+                }
+                if (mazeGrid[row][col].getHasBottomWall()) {
+                    drawBottomWall(row, col);
+                }
+                if (mazeGrid[row][col].getHasLeftWall()) {
+                    drawLeftWall(row, col);
+                }
+                if (mazeGrid[row][col].getHasRightWall()) {
+                    drawRightWall(row, col);
+                }
+            }
+        }
+    }
+
+    private void drawTopWall(int row, int col) {
+        mazeCanvas.drawLine(col*mazeSectionLength,
+                row*mazeSectionLength,
+                (col + 1)*mazeSectionLength,
+                row*mazeSectionLength, mazeBrush);
+    }
+
+    private void drawBottomWall(int row, int col) {
+        mazeCanvas.drawLine(col*mazeSectionLength,
+                (row + 1)*mazeSectionLength,
+                (col + 1)*mazeSectionLength,
+                (row + 1)*mazeSectionLength, mazeBrush);
+    }
+
+    private void drawLeftWall(int row, int col) {
+        mazeCanvas.drawLine(col*mazeSectionLength,
+                row*mazeSectionLength,
+                col*mazeSectionLength,
+                (row + 1)*mazeSectionLength, mazeBrush);
+    }
+
+    private void drawRightWall(int row, int col) {
+        mazeCanvas.drawLine((col + 1)*mazeSectionLength,
+                row*mazeSectionLength,
+                (col + 1)*mazeSectionLength,
+                (row + 1)*mazeSectionLength, mazeBrush);
     }
 
     /**
@@ -103,4 +212,21 @@ public class MazeView extends SurfaceView implements SurfaceHolder.Callback, Maz
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
 
     }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+
+    }
 }
+
