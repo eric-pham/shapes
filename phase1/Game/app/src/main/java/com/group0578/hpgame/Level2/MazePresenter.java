026977f4dcda430af35450fdeea6a2c9ff5ba907
aggar196 <pa.aggarwal@mail.utoronto.ca>
2019-10-29 23:49:52 -0400

    Reorganized classes MazeUseCases.java, MazePresenter.java, and MazeView.java to follow the MVP design pattern and Clean Architecture, and made MazeItem.java the parent class of MagicalObject.java and MazeTrap.java.

--- phase1/Game/app/src/main/java/com/group0578/hpgame/Level2/MazePresenter.java
+++ phase1/Game/app/src/main/java/com/group0578/hpgame/Level2/MazePresenter.java
@@ -1,10 +1,18 @@
 package com.group0578.hpgame.Level2;
 
+import com.group0578.hpgame.Level2.MazeEntities.MazeSection;
+
 public class MazePresenter implements Maze.Presenter {
 
     private Maze.View mazeView;
+    private MazeUseCases mazeUseCases;
 
     public MazePresenter(Maze.View mazeView) {
         this.mazeView = mazeView;
+        this.mazeUseCases = new MazeUseCases();
+    }
+
+    public MazeSection[][] getMazeGrid() {
+        return mazeUseCases.buildMazeGrid();
     }
 }
--- phase1/Game/app/src/main/java/com/group0578/hpgame/Level2/MazeUseCases.java
+++ phase1/Game/app/src/main/java/com/group0578/hpgame/Level2/MazeUseCases.java
@@ -1,4 +1,30 @@
 package com.group0578.hpgame.Level2;
 
+import com.group0578.hpgame.Level2.MazeEntities.MazeSection;
+
 public class MazeUseCases {
+
+    /** The number of rows in the maze. */
+    private static final int ROWS = 4;
+    /** The number of columns in the maze. */
+    private static final int COLS = 4;
+
+    public MazeUseCases() { }
+
+    public MazeSection[][] buildMazeGrid() {
+        MazeSection[][] mazeGrid = new MazeSection[ROWS][COLS];
+        return createMazeGrid(mazeGrid);
+    }
+
+    private MazeSection[][] createMazeGrid(MazeSection[][] mazeGrid) {
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                mazeGrid[row][col] = new MazeSection(row, col);
+            }
+        }
+        return mazeGrid;
+    }
+
+
+
 }
