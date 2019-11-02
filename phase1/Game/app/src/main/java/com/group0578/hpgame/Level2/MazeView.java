bdf4e4e0f6ecc6f6a5b30894d31e5a552c320eaa
aggar196 <pa.aggarwal@mail.utoronto.ca>
2019-11-01 23:18:35 -0400

    Updated MazeView.java by adding java documentation/comments for all methods and instance attributes; Deleted MainMazeThread.java, AppUserAccountsDatabase.java, UserAccount.java, and UserAccountDao.java as these classes were not being used.

--- phase1/Game/app/src/main/java/com/group0578/hpgame/Level2/MazeView.java
+++ phase1/Game/app/src/main/java/com/group0578/hpgame/Level2/MazeView.java
@@ -13,38 +13,105 @@ import android.view.View;
 
 import com.group0578.hpgame.Level2.MazeEntities.MazeSection;
 
-/** The Maze's view or visual appearance on the screen for the user. */
+/**
+ * The Maze's view or visual appearance on the screen for the user.
+ *
+ * Implementing interface Runnable -- allows performing actions on Thread objects.
+ * Implementing View.OnTouchListener -- for performing actions in response to user clicks
+ */
 public class MazeView extends SurfaceView implements Runnable, Maze.View, View.OnTouchListener{
 
+    /**
+     * The thread that begins running the Maze Level the user sees on the screen.
+     */
     Thread mazeThread = null;
+
+    /**
+     * The surfaceHolder containing the surface of this MazeView screen.
+     */
     SurfaceHolder surfaceHolder;
+
+    /**
+     * Whether the Thread mazeThread object is currently running or not.
+     */
     boolean isRunning = false;
 
-    /** The screen width. */
+    /**
+     * The width of the screen representing this MazeView.
+     */
     private int screenWidth;
-    /** The screen height. */
+
+    /**
+     * The height of the screen representing this MazeView.
+     */
     private int screenHeight;
-    /** The array representing the maze; structured as a grid where each element is a MazeSection. */
+
+    /**
+     * The array representing the maze; structured as a grid where each element is a MazeSection.
+     */
     private MazeSection[][] mazeGrid;
-    /** the paint object used for the maze */
+
+    /**
+     * The Paint object used to draw the grid lines of the maze on the screen.
+     */
     private Paint mazeBrush;
 
+    /**
+     * The numerical value for the length of each MazeSection in the mazeGrid array.
+     */
     private float mazeSectionLength;
-    private float verticalMargin;
-    private float horizontalMargin;
-    private MazePresenter mazePresenter;
-    private MazeSection exitPoint;
-    private Paint playerPaint, exitPointPaint;
-    private Paint textBrush = new Paint();
-    private Player player;
-//    private float playerX, playerY;
 
+    /**
+     * The length between the top of the screen and beginning of the Maze.
+     */
+    private float verticalMargin;
+
+    /**
+     * The length between the sides of the screen and the Maze.
+     */
+    private float horizontalMargin;
+
+    /**
+     * An instance of the mazePresenter responsible for handling user's actions.
+     */
+    private MazePresenter mazePresenter;
+
+    /**
+     * The final location in the mazeGrid array the user reaches upon completion of the Maze.
+     */
+    private MazeSection exitPoint;
+
+    /**
+     * The Paint objects used to draw and distinguish the Player's current location from
+     * the exit Point.
+     */
+    private Paint playerPaint, exitPointPaint;
+
+    /**
+     * The Paint object used to draw text on the screen -- used for creating the legend.
+     */
+    private Paint textBrush = new Paint();
+
+    /**
+     * The Player object that is currently playing the Maze.
+     * Used for keeping track of current location in the Maze.
+     */
+    private Player player;
+
+    /**
+     * Construct a new instance of a MazeView.
+     * @param context the environment making this MazeView appear on the screen.
+     */
     public MazeView(Context context) {
         super(context);
+
+        // Initialize instance attributes
         surfaceHolder = getHolder();
         screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
         screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
         mazePresenter = new MazePresenter(this);
+
+        // Prepare the textBrush object to draw text on the screen in run() method
         textBrush.setColor(Color.WHITE);
         textBrush.setStrokeWidth(10);
         textBrush.setTextSize(50);
@@ -55,27 +122,34 @@ public class MazeView extends SurfaceView implements Runnable, Maze.View, View.O
      * to create a thread, starting the thread causes the object's
      * <code>run</code> method to be called in that separately executing
      * thread.
-     * <p>
      * The general contract of the method <code>run</code> is that it may
      * take any action whatsoever.
      *
-     * @see Thread#run()
+     * @see Thread #run()
      */
     @Override
     public void run() {
+
+        // Receiving mazeGrid from Presenter
         mazeGrid = mazePresenter.getMazeGrid();
+        // Initializing new Player object with coordinates 0, 0 in mazeGrid array
         player = new Player(0,0);
-        setPlayerExitLocations();
+
+        setExitLocation();
         prepareMazeBrush();
         determineMazeDimensions();
-        createPlayer();
-        createExitPoint();
+        makePlayer();
+        makeExitPoint();
 
+        // Only draw the Maze when the thread is currently running
         while (isRunning) {
+
+            // If surface is null, then don't draw the maze
             if (!surfaceHolder.getSurface().isValid()) {
                 continue;
             }
 
+            // Creating a canvas object to draw the Maze and other components on the screen
             Canvas mazeCanvas = surfaceHolder.lockCanvas();
             mazeCanvas.drawARGB(255, 100, 30, 250);
             mazeCanvas.drawText("Player = Circle", 100, 100, textBrush);
@@ -83,11 +157,17 @@ public class MazeView extends SurfaceView implements Runnable, Maze.View, View.O
             drawMazeWalls(mazeCanvas);
             drawPlayer(mazeCanvas, mazeSectionLength/10);
             drawExitPoint(mazeCanvas, mazeSectionLength/10);
+
+            // After drawing and performing actions on mazeCanvas, show mazeCanvas on the screen
             surfaceHolder.unlockCanvasAndPost(mazeCanvas);
         }
 
     }
 
+    /**
+     * Stops the current thread from running and sets instance attribute
+     * mazeThread to a null object.
+     */
     public void pause() {
         isRunning = false;
             try {
@@ -98,24 +178,39 @@ public class MazeView extends SurfaceView implements Runnable, Maze.View, View.O
         mazeThread = null;
     }
 
+    /**
+     * Begins running a new thread and sets mazeThread to a new Thread object.
+     */
     public void resume() {
         isRunning = true;
         mazeThread = new Thread(this);
         mazeThread.start();
     }
 
-    private void setPlayerExitLocations() {
+    /**
+     * Initializes the exitPoint attribute to last column and last row in the mazeGrid array.
+     */
+    private void setExitLocation() {
         int rows = mazePresenter.getRowColumnAttributes()[0];
         int cols = mazePresenter.getRowColumnAttributes()[1];
         exitPoint = mazeGrid[rows-1][cols-1];
     }
 
+    /**
+     * Initializes and performs actions on mazeBrush attribute which is used to draw the maze.
+     */
     void prepareMazeBrush() {
-        mazeBrush = new Paint(); // maybe instantiate where declared
+        mazeBrush = new Paint();
         mazeBrush.setColor(Color.WHITE);
         mazeBrush.setStrokeWidth(3);
     }
 
+    /**
+     * Determines the dimensions for the Maze by deciding:
+     *
+     * 1) How large each mazeSection should be on the screen (setting mazeSectionLength attribute)
+     * 2) Vertical and horizontal margins between the maze and edges of the screen
+     */
     void determineMazeDimensions() {
 
         String TAG = "MazeView.determineMazeDimensions";
@@ -123,35 +218,53 @@ public class MazeView extends SurfaceView implements Runnable, Maze.View, View.O
         int rows = mazeGrid.length;
         int cols = mazeGrid[0].length;
 
+        // Phone screen rotated sideways or landscape mode
         if (screenHeight <= screenWidth) {
             mazeSectionLength = screenHeight/(rows + 1);
-        } else {  // screenWidth < screenHeight
+        } else {  // phone screen vertically oriented or portrait mode
             mazeSectionLength = screenWidth/(cols + 1);
         }
 
         float mazeWidth = cols*mazeSectionLength;
         float mazeHeight = rows*mazeSectionLength;
 
+        // Setting margins based on mazeWidth and mazeHeight attributes
         verticalMargin = (screenHeight - mazeHeight)/2;
         horizontalMargin = (screenWidth - mazeWidth)/2;
 
         Log.e(TAG, "screen height, screen width: " + screenHeight + ", " + screenWidth);
     }
 
-    private void createPlayer() {
+    /**
+     * Initializes a paint object used to draw the player.
+     */
+    private void makePlayer() {
         playerPaint = new Paint();
         playerPaint.setColor(Color.RED);
     }
 
-    private void createExitPoint() {
+    /**
+     * Initializes a paint object used to draw the exit point in the maze.
+     */
+    private void makeExitPoint() {
         exitPointPaint = new Paint();
         exitPointPaint.setColor(Color.GREEN);
     }
 
+    /**
+     * Drawing the walls of the maze based on if each mazeSection in the mazeGrid has a
+     * top, bottom, left or right wall
+     * @param mazeCanvas the canvas on which to draw the walls of the maze.
+     */
     void drawMazeWalls(Canvas mazeCanvas) {
         String TAG = "MazeView.drawMazeWalls";
         Log.i(TAG, "Trying to draw walls");
+
+        // translating canvas makes drawing walls easier
+        // moves 'origin' of where objects are drawn on the screen to top-left corner of the maze
         mazeCanvas.translate(horizontalMargin, verticalMargin);
+
+        // Drawing the walls for each of the mazeSections inside mazeGrid array
         for (int row = 0; row < mazeGrid.length; row++) {
             for (int col = 0; col < mazeGrid[0].length; col++) {
                 if (mazeGrid[row][col].getHasTopWall()) {
@@ -168,8 +281,17 @@ public class MazeView extends SurfaceView implements Runnable, Maze.View, View.O
                 }
             }
         }
+
     }
 
+    /**
+     * Drawing a line on the screen representing the top wall of some mazeSection
+     * in the mazeGrid array.
+     *
+     * @param row the row in the mazeGrid of where to draw the top wall
+     * @param col the column in the mazeGrid of where to draw the top wall
+     * @param mazeCanvas the canvas on which to draw the top wall
+     */
     private void drawTopWall(int row, int col, Canvas mazeCanvas) {
         mazeCanvas.drawLine(col*mazeSectionLength,
                 row*mazeSectionLength,
@@ -177,6 +299,14 @@ public class MazeView extends SurfaceView implements Runnable, Maze.View, View.O
                 row*mazeSectionLength, mazeBrush);
     }
 
+    /**
+     * Drawing a line on the screen representing the bottom wall of some mazeSection
+     * in the mazeGrid array.
+     *
+     * @param row the row in the mazeGrid of where to draw the bottom wall
+     * @param col the column in the mazeGrid of where to draw the bottom wall
+     * @param mazeCanvas the canvas on which to draw the bottom wall
+     */
     private void drawBottomWall(int row, int col, Canvas mazeCanvas) {
         mazeCanvas.drawLine(col*mazeSectionLength,
                 (row + 1)*mazeSectionLength,
@@ -184,6 +314,14 @@ public class MazeView extends SurfaceView implements Runnable, Maze.View, View.O
                 (row + 1)*mazeSectionLength, mazeBrush);
     }
 
+    /**
+     * Drawing a line on the screen representing the left wall of some mazeSection
+     * in the mazeGrid array.
+     *
+     * @param row the row in the mazeGrid of where to draw the left wall
+     * @param col the column in the mazeGrid of where to draw the left wall
+     * @param mazeCanvas the canvas on which to draw the left wall
+     */
     private void drawLeftWall(int row, int col, Canvas mazeCanvas) {
         mazeCanvas.drawLine(col*mazeSectionLength,
                 row*mazeSectionLength,
@@ -191,6 +329,14 @@ public class MazeView extends SurfaceView implements Runnable, Maze.View, View.O
                 (row + 1)*mazeSectionLength, mazeBrush);
     }
 
+    /**
+     * Drawing a line on the screen representing the right wall of some mazeSection
+     * in the mazeGrid array.
+     *
+     * @param row the row in the mazeGrid of where to draw the right wall
+     * @param col the column in the mazeGrid of where to draw the right wall
+     * @param mazeCanvas the canvas on which to draw the right wall
+     */
     private void drawRightWall(int row, int col, Canvas mazeCanvas) {
         mazeCanvas.drawLine((col + 1)*mazeSectionLength,
                 row*mazeSectionLength,
@@ -198,6 +344,11 @@ public class MazeView extends SurfaceView implements Runnable, Maze.View, View.O
                 (row + 1)*mazeSectionLength, mazeBrush);
     }
 
+    /**
+     * Drawing a circle on the screen representing the player's current location in the maze.
+     * @param mazeCanvas the canvas on which to draw the circle/player
+     * @param margin the margin between the player and the maze walls.
+     */
     private void drawPlayer(Canvas mazeCanvas, float margin) {
         if (player.hasMoved()) {
             float left = player.getCol() * mazeSectionLength + margin;
@@ -215,6 +366,11 @@ public class MazeView extends SurfaceView implements Runnable, Maze.View, View.O
 
     }
 
+    /**
+     * Drawing a rectangle on the screen representing the exit point location in the maze.
+     * @param mazeCanvas the canvas on which to draw the square/exit location.
+     * @param margin the margin between the exitPoint location and the maze walls.
+     */
     private void drawExitPoint(Canvas mazeCanvas, float margin) {
         float left = exitPoint.getCol() * mazeSectionLength + margin;
         float right = (exitPoint.getCol() + 1) * mazeSectionLength - margin;
@@ -224,31 +380,39 @@ public class MazeView extends SurfaceView implements Runnable, Maze.View, View.O
     }
 
     /**
-     * Called when a touch event is dispatched to a view. This allows listeners to
-     * get a chance to respond before the target view.
+     * Called when a touch event is dispatched to a view. This allows users to click/drag the
+     * player on the screen.
      *
      * @param v The view the touch event has been dispatched to.
-     * @param event The MotionEvent object containing full information about
-     *              the event.
+     * @param event The MotionEvent object containing full information about the event.
      * @return True if the listener has consumed the event, false otherwise.
      */
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        if (!player.hasMoved()) {
-            player.setHasMoved(true);
-        }
 
+        // If the user clicks and drags the mouse, then an action has been detected
         if (event.getAction() == MotionEvent.ACTION_MOVE) {
+
+            if (!player.hasMoved()) {
+                player.setHasMoved(true); // Action move event detected so player moved.
+            }
+
+            // Gets the x and y coordinates of where the user clicked on the screen.
             float touchX = event.getX(), touchY = event.getY();
 
+            // Player's current x and y locations in the Maze
             float currPlayerX = horizontalMargin + (player.getCol()+0.5f) * mazeSectionLength;
             float currPlayerY = verticalMargin + (player.getRow()+0.5f) * mazeSectionLength;
 
+            // The difference between where the user clicked and player's location
             float displacementX = touchX - currPlayerX, displacementY = touchY - currPlayerY;
 
             float absDisplacementX = Math.abs(displacementX),
                     absDisplacementY = Math.abs(displacementY);
 
+            // Checks if user clicked/dragged "far enough" to move the player
+            // Only when the user clicks/drags the cursor a length larger than mazeSectionLength
+            // does the player on the screen begin to move.
             if (absDisplacementX > mazeSectionLength || absDisplacementY > mazeSectionLength) {
 
                 if (absDisplacementX > absDisplacementY) {  // move horizontally
@@ -272,32 +436,43 @@ public class MazeView extends SurfaceView implements Runnable, Maze.View, View.O
             return true;
         }
 
-        return true;
+        return true; // returns true so the user can continue dragging/clicking to move the player
     }
 
+    /**
+     * Moves the player's current location up which means one row higher in the mazeGrid array.
+     */
     private void movePlayerUp() {
         if (!mazeGrid[player.getRow()][player.getCol()].getHasTopWall()) {
             player.setRow(player.getRow() - 1);
         }
     }
 
+    /**
+     * Moves the player's current location down which means one row larger in the mazeGrid array.
+     */
     private void movePlayerDown() {
         if (!mazeGrid[player.getRow()][player.getCol()].getHasBottomWall()) {
             player.setRow(player.getRow() + 1);
         }
     }
 
+    /**
+     * Moves the player's current location left: One column smaller in the mazeGrid array
+     */
     private void movePlayerLeft() {
         if (!mazeGrid[player.getRow()][player.getCol()].getHasLeftWall()) {
             player.setCol(player.getCol() - 1);
         }
     }
 
+    /**
+     * Moves the player's current location right: One column larger in the mazeGrid array
+     */
     private void movePlayerRight() {
         if (!mazeGrid[player.getRow()][player.getCol()].getHasRightWall()) {
             player.setCol(player.getCol() + 1);
         }
     }
-
 
 }
