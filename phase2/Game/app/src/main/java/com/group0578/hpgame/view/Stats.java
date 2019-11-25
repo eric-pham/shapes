c236dd510703c207cdea78fd5c76c108b75513b1
VG <vassilisa.gotcheva@mail.utoronto.ca>
2019-11-24 23:50:48 -0500

    Renamed activity_user_stats.xml to activity_stats.xml; Renamed the associated java files to match the naming scheme, in other words, I replaced 'UserStats' with 'Stats'.

---
+++ phase2/Game/app/src/main/java/com/group0578/hpgame/view/Stats.java
@@ -0,0 +1,32 @@
+package com.group0578.hpgame.view;
+
+import com.group0578.hpgame.presenter.StatsPresenter;
+
+/**
+ * Defines the contract between the View {@link StatsActivity} and the
+ * Presenter {@link StatsPresenter}.
+ */
+public interface Stats {
+
+    /**
+     * Behaviour of the StatsActivity.
+     */
+    interface View {
+
+
+    }
+
+    /**
+     * Not sure if needed yet.
+     */
+    interface Model {
+
+    }
+
+    /**
+     * Behaviour of the StatsPresenter.
+     */
+    interface Presenter {
+
+    }
+}
--- phase2/Game/app/src/main/java/com/group0578/hpgame/view/StatsActivity.java
+++ phase2/Game/app/src/main/java/com/group0578/hpgame/view/StatsActivity.java
@@ -5,17 +5,17 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.group0578.hpgame.R;
-import com.group0578.hpgame.presenter.UserStatsPresenter;
+import com.group0578.hpgame.presenter.StatsPresenter;
 
 /**
  * Displays the user statistics screen.
  */
-public class UserStatsActivity extends AppCompatActivity implements UserStats.View {
+public class StatsActivity extends AppCompatActivity implements Stats.View {
 
     /**
      * The presenter associated with this View that handles the user's interactions with the UI.
      */
-    private UserStatsPresenter userStatsPresenter;
+    private StatsPresenter statsPresenter;
 
     /**
      * The constructor for the activity that displays the user statistics screen.
@@ -23,8 +23,8 @@ public class UserStatsActivity extends AppCompatActivity implements UserStats.Vi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_stats);
-        userStatsPresenter = new UserStatsPresenter(this);
+        setContentView(R.layout.activity_stats);
+        statsPresenter = new StatsPresenter(this);
 
     }
 }
