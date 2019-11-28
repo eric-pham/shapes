a0185f6cc9bc9665be53321699e693f3ef9b9008
Henry Wang <henryhe.wang@mail.utoronto.ca>
2019-11-28 17:45:43 -0500

    Refractored FlyingPresenter, FlyingView to use FlyingInteractor instead of directly accessing database. Added/fixed java docs. Completed & refractored FlyingInteractor.

--- phase2/Game/app/src/main/java/com/group0578/hpgame/Level1/FlyingInteractor.java
+++ phase2/Game/app/src/main/java/com/group0578/hpgame/Level1/FlyingInteractor.java
@@ -3,29 +3,61 @@ package com.group0578.hpgame.Level1;
 import com.group0578.hpgame.model.SQLiteHelper;
 
 public class FlyingInteractor {
+    /**
+     * String representing the theme, either 'Light' or 'Dark'
+     */
+    private String theme;
 
-    private SQLiteHelper sqLiteHelper;
-    private String username;
 
-    FlyingInteractor(SQLiteHelper sqlHelper, String username)
-    {
-        this.sqLiteHelper = sqlHelper;
-        this.username = username;
+    /**
+     * int representing the number of lives
+     */
+    private int lives;
+
+    /**
+     * String representing the theme, either 'Circle' or 'Square'
+     */
+    private String character;
+
+    /**
+     * Create a new FlyingInteractor with the given SQLiteHelper and username. This class stores
+     * and returns the user's information from the database required to run the level.
+     *
+     * @param sqlHelper Helper to get data from database.
+     * @param username  username of the user we're storing info from.
+     */
+    FlyingInteractor(SQLiteHelper sqlHelper, String username) {
+        this.theme = sqlHelper.findColourScheme(username);
+        this.lives = sqlHelper.findLives(username);
+        this.character = sqlHelper.findCharacter(username);
     }
 
-    // Get lives from the data base and return it as an int
-    public int getLives(){
-        return 0;
+
+    /**
+     * Get lives from the data base and return it as an int
+     *
+     * @return number of lives
+     */
+    public int getLives() {
+        return this.lives;
     }
 
-    // Get the theme from the data base and return it as a string
-    public String getTheme(){
-        return "temp";
+    /**
+     * Get the theme from the data base and return it as a string
+     *
+     * @return a String: 'Light' or 'Dark'.
+     */
+    public String getTheme() {
+        return this.theme;
     }
 
-    // Get the character the user selected
-    public String getCharacter(){
-        return "temp";
+    /**
+     * Get the character the user selected
+     *
+     * @return a String: 'Circle' or 'Square'.
+     */
+    public String getCharacter() {
+        return this.character;
     }
 
     // Need to add update database methods for when game is over, will add after transitions are complete
